2c300aefab1b03aef041b175d163c7b365e44196
rbamarc <rbamarc929@gmail.com>
2023-02-06 00:46:21 -0800

    first commit

---
+++ src/main/java/netbeans/positivenegative/PositiveNegative.java
@@ -0,0 +1,27 @@
+
+package netbeans.positivenegative;
+import java.util.Scanner;
+
+/**
+ *
+ * @author halfrikan
+ */
+public class PositiveNegative {
+
+    public static void main(String[] args) {
+        int number = 0;
+        String stringValue = "";
+        
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Enter a number");
+        stringValue = inputReader.nextLine();
+        number = Integer.parseInt(stringValue);
+        
+        if (number >= 0) {
+            System.out.println("the number is positive");
+        } else {
+            System.out.println("the number is negative");
+        }
+            
+    }
+}
